5a716f3bcd4fe8118047148d860a470b678c9fa3
robo <robomarej@gmail.com>
2017-09-10 00:53:36 +0200

    name error

--- section-3/21-runtime-errors.py
+++ section-3/21-runtime-errors.py
@@ -13,3 +13,9 @@ b = "2"
 # TypeError: unsupported operand type(s) for +: 'int' and 'str'
 # print(a + b)
 # print(a + int(b)) # correct
+
+# Traceback (most recent call last):
+#   File "21-runtime-errors.py", line 21, in <module>
+#     print(c)
+# NameError: name 'c' is not defined
+print(c)
