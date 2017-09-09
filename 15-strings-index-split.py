4eb300e5bda74c6f3227c9abb92efc7bf7befce3
robo <robomarej@gmail.com>
2017-09-09 23:07:15 +0200

    indexes in string

---
+++ 15-strings-index-split.py
@@ -0,0 +1,8 @@
+c = "Hi there!"
+print(c[3]) # "t"
+print(c[len(c)-1]) # "!"
+print(c[0:1]) # "H" - upper bound is not included !
+print(c[0:2]) # "Hi"
+print(c[:2]) # "Hi" - nothing works as 0
+print(c[:]) # "Hi there!" - nothing works as 0
+print(c[3:]) # "Hi there!" - nothing works as 0 (shorter than print(c[len(c)-1]) # "!")
