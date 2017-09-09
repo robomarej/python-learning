2a656a3b83d6dc90a208c0752364844032578c0f
robo <robomarej@gmail.com>
2017-09-09 23:36:59 +0200

    different types in dictionary

--- 18-dictionaries.py
+++ 18-dictionaries.py
@@ -1,3 +1,14 @@
 d = {"surname":"Smith", "name":"John"}
 print(d) # {'surname': 'Smith', 'name': 'John'}
 print(d["name"]) # John
+d = {
+    "surname":"Smith",
+    "name":"John",
+    "cities":(
+        "Porto",
+        "San Diego",
+        "Bali"
+    ),
+    "age": 20
+}
+print(d) # {'name': 'John', 'age': 20, 'cities': ('Porto', 'San Diego', 'Bali'), 'surname': 'Smith'}
