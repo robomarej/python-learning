934eecc3b11712fd968de55f9541c603755ed70d
robo <robomarej@gmail.com>
2017-09-10 00:39:27 +0200

    syntax error - specific

--- section-3/20-errors.py
+++ section-3/20-errors.py
@@ -2,5 +2,12 @@
 #     int 9
 #         ^
 # SyntaxError: invalid syntax
-int 9
-int(9) # correct
+# int 9
+# int(9) # correct
+
+# File "20-errors.py", line 12
+#     print 3
+#           ^
+# SyntaxError: Missing parentheses in call to 'print'
+print 3
+print(3)
