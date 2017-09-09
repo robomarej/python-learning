1f2c09b9ae15378a45f6b33fe8fcf9472022a273
robo <robomarej@gmail.com>
2017-09-10 01:34:40 +0200

    function default arguments

--- section-4/27-functions-advanced.py
+++ section-4/27-functions-advanced.py
@@ -1,4 +1,9 @@
-def minutes_to_hours(minutes=70, seconds):
+# def minutes_to_hours(minutes=70, seconds):
+#     hours = minutes / 60 + seconds / 3600
+#     # hours = minutes / 60.0 # in python 2
+#     return hours
+
+def minutes_to_hours(seconds, minutes=70):
     hours = minutes / 60 + seconds / 3600
     # hours = minutes / 60.0 # in python 2
     return hours
@@ -15,4 +20,7 @@ print(minutes_to_hours(60, 300))
 #     def minutes_to_hours(minutes=70, seconds):
 #                         ^
 # SyntaxError: non-default argument follows default argument
-print(minutes_to_hours(60))
+# print(minutes_to_hours(60))
+
+print(minutes_to_hours(60, 300)) #overrides default value for minutes (300 instead of 70)
+print(minutes_to_hours(60)) # uses default value for minutes (70)
