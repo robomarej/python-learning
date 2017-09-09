c3beda8d8d7d0a85d3b67cca58644d96b9711b9e
robo <robomarej@gmail.com>
2017-09-09 23:23:26 +0200

    appending and removing from lists

--- 16-list.py
+++ 16-list.py
@@ -3,3 +3,9 @@ print(c)
 print(type(c)) # <class 'list'>
 print(c[:2]) # ['H', 2]
 print(type(c[:2])) # <class 'list'> List spling is also of type list
+c.append(3)
+print(c) # ['H', 2, 'Hello', 3] - lists are not immutable
+c.remove(3)
+print(c) # removes FIRST OCCURENCE
+print(c.remove(2)) # None - remove doesn't have return value
+print(c) # ['H', 'Hello']
