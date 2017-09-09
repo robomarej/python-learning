ab873813da2ef20f0e9c455839c0ea84de0fb1d4
robo <robomarej@gmail.com>
2017-09-09 23:10:30 +0200

    negative indexes in strings

--- 15-strings-index-split.py
+++ 15-strings-index-split.py
@@ -6,3 +6,5 @@ print(c[0:2]) # "Hi"
 print(c[:2]) # "Hi" - nothing works as 0
 print(c[:]) # "Hi there!" - nothing works as 0
 print(c[3:]) # "Hi there!" - nothing works as 0 (shorter than print(c[len(c)-1]) # "!")
+print(c[-1:]) # ! negative indexes start from the end
+print(c[-6:-1]) # negative indexes start from the end
