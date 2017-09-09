a612f727ac014c868a51b1bcc4eae372f3b36ab3
robo <robomarej@gmail.com>
2017-09-10 01:31:24 +0200

    default values for arguments - incorrect order

--- section-4/27-functions-advanced.py
+++ section-4/27-functions-advanced.py
@@ -1,4 +1,4 @@
-def minutes_to_hours(minutes, seconds):
+def minutes_to_hours(minutes=70, seconds):
     hours = minutes / 60 + seconds / 3600
     # hours = minutes / 60.0 # in python 2
     return hours
@@ -9,4 +9,10 @@ print(minutes_to_hours(60, 300))
 #   File "27-functions-advanced.py", line 12, in <module>
 #     print(minutes_to_hours(60))
 # TypeError: minutes_to_hours() missing 1 required positional argument: 'seconds'
+# print(minutes_to_hours(60))
+
+# File "27-functions-advanced.py", line 1
+#     def minutes_to_hours(minutes=70, seconds):
+#                         ^
+# SyntaxError: non-default argument follows default argument
 print(minutes_to_hours(60))
