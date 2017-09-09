11cb75d10f02a1f037246ccd9b3da0e9e5e9e143
robo <robomarej@gmail.com>
2017-09-10 01:53:15 +0200

    if elif else

--- section-4/34-conditionals.py
+++ section-4/34-conditionals.py
@@ -1,6 +1,10 @@
 # a = 5
-a = 4
+# a = 4
+a = 6
+
 if a < 5:
     print("a is less than 5")
+elif a == 5:
+    print("a is equal to 5")
 else:
-    print("a is equal or greater than 5")
+    print("a is greater than 5")
