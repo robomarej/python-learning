ee875bce6038c2377f8f7b806443bb30c270c503
robo <robomarej@gmail.com>
2017-09-10 03:18:10 +0200

    reading the file

--- section-6/46-open-read-file.py
+++ section-6/46-open-read-file.py
@@ -1,2 +1,10 @@
 file=open("46-example.txt", "r")
-print(type(file))
+content = file.read()
+print(content)
+content = file.readlines() # prints nothing because the pointer is at the end of the file becouse of the previous file.read()
+print(content)
+file.seek(0) # reset pointer in file
+content = file.readlines()
+print(content)
+content = [i.rstrip("\n") for i in content]
+print(content)
