06577a4e8f328835496c36db450dac4f8eaaa06e
robo <robomarej@gmail.com>
2017-09-10 04:31:38 +0200

    docstrings

--- section-7/55-comment-document-code.py
+++ section-7/55-comment-document-code.py
@@ -1,6 +1,13 @@
+"""
+This script creates an empty file
+"""
+
 filename="sample.txt"
 
 # Create empty file
 def create_file():
+    """
+    This function creates empty file
+    """
     with open(filename,"w") as file:
         file.write("") # Writing empty string
