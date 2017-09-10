c7b0a8dfc76cbb98fcfbe9059b6cbfc613d8c895
robo <robomarej@gmail.com>
2017-09-10 04:24:25 +0200

    importing module + pip

--- section-7/54-modules-libs-packages.py
+++ section-7/54-modules-libs-packages.py
@@ -1,5 +1,6 @@
 import os # import module
 import sqlite3 # import library
+import glob # import package glob after pip install glob
 print(os.listdir())
 print(os.path.dirname(os.__file__))
-print(os.listdir(os.path.dirname(os.__file__)))
+# print(os.listdir(os.path.dirname(os.__file__)))
