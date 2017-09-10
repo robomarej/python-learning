cfd6dd4c05e91462f01ccdaf992c4ca9c7aa388c
robo <robomarej@gmail.com>
2017-09-10 03:28:45 +0200

    adding lines to file via for loop

--- section-6/47-open-write-file.py
+++ section-6/47-open-write-file.py
@@ -9,3 +9,10 @@ file.close()
 file = open("47-example.txt", "w")
 file.write("Line 1\nLine 2")
 file.close()
+
+
+lines = ["Line 1", "Line 2", "Line 3"]
+file = open("47-example.txt", "w")
+for line in lines:
+    file.write(line+"\n")
+file.close()
