aa74a02e47dcfa3bd0b376e21f7bd86edad65421
robo <robomarej@gmail.com>
2017-09-10 13:46:57 +0200

    optimizing output message

--- section-8/app1.py
+++ section-8/app1.py
@@ -17,4 +17,11 @@ def translate(word):
         return "The word doesn't exist. Please double check it."
 
 word = input("Enter word: ")
-print(translate(word))
+
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
