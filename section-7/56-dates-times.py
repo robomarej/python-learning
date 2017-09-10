4f9afb4acc38270424426618f8216826e5efca6f
robo <robomarej@gmail.com>
2017-09-10 04:58:57 +0200

    various date time operation

--- section-7/56-dates-times.py
+++ section-7/56-dates-times.py
@@ -1,2 +1,17 @@
 import datetime
 print(datetime.datetime.now())
+
+yesterday=datetime.datetime(2017,9,9,4,0,0,0)
+now = datetime.datetime.now()
+delta = now - yesterday
+print(delta)
+print(delta.total_seconds())
+
+filename=now.strftime("%Y-%m-%d-%H-%M-%S-%f")+"-sample.txt"
+def create_file():
+    with open(filename,"w") as file:
+        file.write("") # Writing empty string
+
+create_file()
+
+print(now+datetime.timedelta(days=2)) # adding 2 days
