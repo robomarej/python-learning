81b6cd1d8baef95bafc94ce6b2e0c9b42d9d7cc9
robo <robomarej@gmail.com>
2017-09-10 13:34:06 +0200

    accounting for close matches

--- section-8/app1.py
+++ section-8/app1.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -6,6 +7,12 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input("Did you mean %s instead? (y/n): " % get_close_matches(word, data.keys())[0])
+        if yn == "y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == "n":
+            return "The word doesn't exist. Please double check it."
     else:
         return "The word doesn't exist. Please double check it."
 
