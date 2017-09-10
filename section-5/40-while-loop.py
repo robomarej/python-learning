01b67dc0786bd4dbb8e30c0916ad407f55b67cbc
robo <robomarej@gmail.com>
2017-09-10 02:33:04 +0200

    currency converter from user input

--- section-5/40-while-loop.py
+++ section-5/40-while-loop.py
@@ -1,2 +1,8 @@
-planet = input("What planet are you from: ")
-print(planet)
+def currency_converter(rate, euros):
+    dollars = euros*rate
+    return dollars
+
+r = float(input("Enter rate: "))
+e = float(input("Enter euros: "))
+
+print(currency_converter(r, e))
